d3da0354b2239db57ad453de3bea4e54d394b905
James Mintram <me@jamesrm.com>
2021-11-27 23:17:16 +0000

    libext2: Add an ephemeral test_huge.img (contains > 1 group descriptor)

--- tools/ext2fs/data/mkdata.sh
+++ tools/ext2fs/data/mkdata.sh
@@ -6,3 +6,5 @@ genext2fs -b 4096 -d ./test3/ test3.img
 
 genext2fs -B 4096 -b 16 -d ./test1/ test1_4kb.img
 genext2fs -B 2048 -b 256 -d ./test2/ test2_2kb.img
+
+genext2fs -B 1024 -b 16384 -d ./test1/ test_huge.img
