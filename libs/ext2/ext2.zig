4befa9adc5f5b16a4b8fc4b1ee6488df4826f397
James Mintram <me@jamesrm.com>
2021-11-27 23:05:08 +0000

    libext2: parse block group descriptor and print

--- libs/ext2/ext2.zig
+++ libs/ext2/ext2.zig
@@ -1,3 +1,49 @@
+pub const Ext2_DirectoryEntry = extern struct {
+    inode: u32,
+    rec_len: u16,
+    name_len: u8,
+    file_type: u8,
+    name: [255]u8,
+};
+
+pub const Ext2_InodeTableEntry = extern struct {
+    i_mode: u16,
+    i_uid: u16,
+    i_size: u32,
+    i_atime: u32,
+    i_ctime: u32,
+    i_mtime: u32,
+    i_dtime: u32,
+    i_gid: u16,
+    i_links_count: u16,
+    i_blocks: u32,
+    i_flags: u32,
+    i_osd1: u32,
+
+    i_block: [12]u32,
+    i_block_indirect: u32,
+    i_block_double_indirect: u32,
+    i_block_triple_indirect: u32,
+
+    i_generation: u32,
+    i_file_acl: u32,
+    i_dir_acl: u32,
+    i_faddr: u32,
+    i_osd2: [12]u8,
+};
+
+pub const Ext2_BlockGroupDescriptor = extern struct {
+    bg_block_bitmap: u32,
+    bg_inode_bitmap: u32,
+    bg_inode_table: u32,
+    bg_free_blocks_count: u16,
+    bg_free_inodes_count: u16,
+
+    bg_used_dirs_count: u16,
+    bg_pad: u16,
+    bg_reserved: [12]u8,
+};
+
 pub const Ext2_SuperBlock = extern struct {
     s_inodes_count: u32,
     s_blocks_count: u32,
@@ -34,20 +80,58 @@ pub const Ext2_SuperBlock = extern struct {
 
     s_def_resuid: u16,
     s_def_resgid: u16,
+
+    pub fn block_size(self: *const Ext2_SuperBlock) u32 {
+        return @intCast(u32, 1024) << @intCast(u4, self.s_log_block_size);
+    }
+
+    pub fn block_index_for_block_group_descriptor(self: *const Ext2_SuperBlock, block_group_index: u32) u32 {
+        return self.s_first_data_block + block_group_index;
+    }
+
+    pub fn block_offset_for_block_group_descriptor(self: *const Ext2_SuperBlock, block_index: u32) u32 {
+        return block_index * self.block_size();
+    }
 };
 
 pub const FS = struct {
-    super_block: Ext2_SuperBlock = undefined,
+    //FIXME: Remove parse_source: anytype param
+    pub fn superblock(self: *const FS, parse_source: anytype) !Ext2_SuperBlock {
+        _ = self;
 
-    pub fn read(parse_source: anytype) !FS {
+        //FIXME: Improve this, can we write directly into the struct?
         var super_block_buf: [@sizeOf(Ext2_SuperBlock)]u8 align(@alignOf(Ext2_SuperBlock)) = undefined;
-
-        // Read superblock from stream
         try parse_source.seekableStream().seekTo(1024);
         try parse_source.reader().readNoEof(&super_block_buf);
-
         var super_block = @ptrCast(*const Ext2_SuperBlock, &super_block_buf);
 
-        return @as(FS, .{ .super_block = super_block.* });
+        //FIXME: Pass in an *out* param?
+        return super_block.*;
+    }
+
+    //FIXME: Remove parse_source: anytype param
+    pub fn block_descriptor_at(self: *const FS, parse_source: anytype, block_group_index: u32) !Ext2_BlockGroupDescriptor {
+        //FIXME: Assert that block_group_index > 0 (as zero is superblock) (TEST)
+
+        var super_block = try self.superblock(parse_source);
+        var block_index = super_block.block_index_for_block_group_descriptor(block_group_index);
+
+        //FIXME: Assert that block_index < self.s_blocks_count (TEST)
+
+        var block_position = super_block.block_offset_for_block_group_descriptor(block_index);
+
+        //FIXME: Improve this, can we write directly into the struct?
+        var block_group_descriptor_buf: [@sizeOf(Ext2_BlockGroupDescriptor)]u8 align(@alignOf(Ext2_BlockGroupDescriptor)) = undefined;
+        try parse_source.seekableStream().seekTo(block_position);
+        try parse_source.reader().readNoEof(&block_group_descriptor_buf);
+        var block_group_descriptor = @ptrCast(*const Ext2_BlockGroupDescriptor, &block_group_descriptor_buf);
+
+        //FIXME: Pass in an *out* param?
+        return block_group_descriptor.*;
+    }
+
+    pub fn mount(parse_source: anytype) !FS {
+        _ = parse_source;
+        return @as(FS, .{});
     }
 };
--- tools/ext2fs/src/main.zig
+++ tools/ext2fs/src/main.zig
@@ -4,9 +4,23 @@ const ext2 = @import("ext2");
 // Doc: https://www.nongnu.org/ext2-doc/ext2.html#s-magic
 
 // FIXME: What do we pass in here? Look at elf+dwarf parser for inspiration?
-pub fn dumpInfo(fs: *ext2.FS) void {
-    std.log.info("Magic {} 0x{X}", .{ @offsetOf(ext2.Ext2_SuperBlock, "s_magic"), fs.super_block.s_magic });
-    //kprint.write("Dump out info for ext2 drive here\r");
+pub fn printSuperBlock(super_block: *const ext2.Ext2_SuperBlock) void {
+    std.log.info("FS Info:", .{});
+    std.log.info("\tMagic {} 0x{X}", .{ @offsetOf(ext2.Ext2_SuperBlock, "s_magic"), super_block.s_magic });
+    std.log.info("\tBlock size {}", .{super_block.block_size()});
+    std.log.info("\tFirst block {}", .{super_block.s_first_data_block});
+}
+
+pub fn printBlockGroupDescriptor(block_group_descriptor: *const ext2.Ext2_BlockGroupDescriptor) void {
+
+    // TODO: Move this into a generic "format" fn and into the Ext2 lib, then info("{}", .{block_group_descriptor})
+    std.log.info("Block Desc Group:", .{});
+    std.log.info("\tblock_bitmap:         {}", .{block_group_descriptor.bg_block_bitmap});
+    std.log.info("\tbg_inode_bitmap:      {}", .{block_group_descriptor.bg_inode_bitmap});
+    std.log.info("\tbg_inode_table        {}", .{block_group_descriptor.bg_inode_table});
+    std.log.info("\tbg_free_blocks_count: {}", .{block_group_descriptor.bg_free_blocks_count});
+    std.log.info("\tbg_free_inodes_count: {}", .{block_group_descriptor.bg_free_inodes_count});
+    std.log.info("\tbg_used_dirs_count:   {}", .{block_group_descriptor.bg_used_dirs_count});
 }
 
 pub fn main() anyerror!void {
@@ -19,9 +33,10 @@ pub fn main() anyerror!void {
         return;
     };
 
-    var fs = try ext2.FS.read(f);
+    var fs = try ext2.FS.mount(f);
 
-    dumpInfo(&fs);
+    printSuperBlock(&try fs.superblock(f));
+    printBlockGroupDescriptor(&try fs.block_descriptor_at(f, 1));
 
     // ext2.std.log.info("All your codebase are belong to us.", .{});
     //TODO: Load a
