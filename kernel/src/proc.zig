b12383d76ce835b16c75bda23bfd434223ec2a53
James Mintram <me@jamesrm.com>
2021-11-27 19:15:55 +0000

    Style cleanup

--- kernel/src/proc.zig
+++ kernel/src/proc.zig
@@ -5,22 +5,20 @@ const thread = @import("thread.zig");
 const address_space = @import("vm/address_space.zig");
 
 pub const Process = struct {
-    next : ?*Process,
-    prev : ?*Process,
+    next: ?*Process,
+    prev: ?*Process,
 
-    pid : u32,
+    pid: u32,
 
-    thread_list : ?*thread.Thread,
-    address_sapce : ?*address_space.AddressSpace,
+    thread_list: ?*thread.Thread,
+    address_sapce: ?*address_space.AddressSpace,
 };
 
-
-pub fn create_process (allocator : *Allocator) !*Process
-{
+pub fn createProcess(allocator: *Allocator) !*Process {
     var newProcess = try allocator.create(Process);
     newProcess.next = undefined;
     newProcess.prev = undefined;
-    
+
     newProcess.pid = 1;
 
     return newProcess;
--- kernel/src/thread.zig
+++ kernel/src/thread.zig
@@ -4,31 +4,30 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 pub const CPUFrame = packed struct {
-    tf_sp:      u64,       //0
-    tf_lr:      u64,       //8
-    tf_elr:     u64,       //16
-    tf_spsr:    u32,       //24
-    tf_esr:     u32,       //28
-    tf_x:       [30]u64,   //32    
+    tf_sp: u64, //0
+    tf_lr: u64, //8
+    tf_elr: u64, //16
+    tf_spsr: u32, //24
+    tf_esr: u32, //28
+    tf_x: [30]u64, //32
 };
 
 pub const Thread = struct {
-    next : ?*Thread,
-    prev : ?*Thread,
+    next: ?*Thread,
+    prev: ?*Thread,
 
-    proc : *process.Process,
+    proc: *process.Process,
 
-    tid : u32,
+    tid: u32,
 
     // Kernel Stack PTR
-    stack_pointer : usize,
+    stack_pointer: usize,
 
     // Alignment better done some other way?
-    kernel_stack : [4096]u8 align(16),
+    kernel_stack: [4096]u8 align(16),
 };
 
-
-pub fn create_default_thread(allocator : *Allocator, parent : *process.Process) !*Thread {
+pub fn create_default_thread(allocator: *Allocator, parent: *process.Process) !*Thread {
     var newThread = try allocator.create(Thread);
 
     newThread.tid = 2;
@@ -40,17 +39,17 @@ pub fn create_default_thread(allocator : *Allocator, parent : *process.Process) 
     return newThread;
 }
 
-var thread_list : *Thread = undefined;
+var thread_list: *Thread = undefined;
 
-fn add_to_thread_list(new_thread : *Thread) void {
+fn add_to_thread_list(new_thread: *Thread) void {
     new_thread.next = thread_list;
     new_thread.prev = undefined;
 
     thread_list = new_thread;
 }
 
-pub fn create_initial_thread(allocator : *Allocator, thread_fn : fn () void) !*Thread {
-    var kernel_proc = process.create_process(allocator) catch unreachable;
+pub fn create_initial_thread(allocator: *Allocator, thread_fn: fn () void) !*Thread {
+    var kernel_proc = process.createProcess(allocator) catch unreachable;
 
     var newThread = try allocator.create(Thread);
     newThread.tid = 1;
@@ -60,20 +59,20 @@ pub fn create_initial_thread(allocator : *Allocator, thread_fn : fn () void) !*T
     newThread.prev = undefined;
 
     // Stack should start from the END of the array
-    newThread.stack_pointer = @ptrToInt(&newThread.kernel_stack[newThread.kernel_stack.len -1]) - @sizeOf(CPUFrame);
-    
+    newThread.stack_pointer = @ptrToInt(&newThread.kernel_stack[newThread.kernel_stack.len - 1]) - @sizeOf(CPUFrame);
+
     var frame = @intToPtr(*CPUFrame, newThread.stack_pointer);
-    
+
     frame.tf_elr = 0;
     frame.tf_lr = @ptrToInt(thread_fn);
     frame.tf_sp = newThread.stack_pointer;
 
-    var spsr : u32 = 0;
-    spsr |= 1 << 0;     // Dunno what this does..
-    spsr |= 1 << 2;     // .M[3:2] = 0b100 -->  Return to EL1
-    spsr |= 1 << 6;     // FIQ masked
-    spsr |= 1 << 7;     // IRQ masked
-    spsr |= 1 << 8;     // SError (System Error) masked
+    var spsr: u32 = 0;
+    spsr |= 1 << 0; // Dunno what this does..
+    spsr |= 1 << 2; // .M[3:2] = 0b100 -->  Return to EL1
+    spsr |= 1 << 6; // FIQ masked
+    spsr |= 1 << 7; // IRQ masked
+    spsr |= 1 << 8; // SError (System Error) masked
     spsr |= 1 << 9;
 
     frame.tf_spsr = spsr;
@@ -84,10 +83,12 @@ pub fn create_initial_thread(allocator : *Allocator, thread_fn : fn () void) !*T
 }
 
 pub fn current_thread() *Thread {
-    var thread_id :usize = 0;
+    var thread_id: usize = 0;
 
     //FIXME - I think this works only by accident
-    thread_id = asm volatile ("mrs %[thread_id], tpidr_el1": [thread_id] "=&r" (-> usize):);
+    thread_id = asm volatile ("mrs %[thread_id], tpidr_el1"
+        : [thread_id] "=&r" (-> usize),
+    );
     return @intToPtr(*Thread, thread_id);
 }
 
@@ -99,13 +100,13 @@ pub fn yield() void {
     switch_to(next);
 }
 
-extern fn _ctx_switch_to_initial(sp : usize) void;
-pub fn switch_to_initial(thread : *Thread) void {
+extern fn _ctx_switch_to_initial(sp: usize) void;
+pub fn switch_to_initial(thread: *Thread) void {
     _ctx_switch_to_initial(@ptrToInt(thread));
 }
 
-extern fn _ctx_switch_to(old_thread : usize, new_thread : usize) void;
-pub fn switch_to(thread : *Thread) void {
+extern fn _ctx_switch_to(old_thread: usize, new_thread: usize) void;
+pub fn switch_to(thread: *Thread) void {
     // Switch to new user space proc address space
     // Call the platform code for switch_to_thread
     // - Update the current ThreadID register value
@@ -115,11 +116,10 @@ pub fn switch_to(thread : *Thread) void {
 
     //TODO: Check they are not the same, otherwise we just instant return
 
-    if (current != thread)
-    {
+    if (current != thread) {
         _ctx_switch_to(@ptrToInt(current), @ptrToInt(thread));
     }
-    
+
     // uart.write("Switching to process {} thread {}\n", .{thread.proc.pid, thread.tid});
-    
+
 }
